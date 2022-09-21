42e012c4a8fddaf848cb4ad99e393411b0e31ad6
Bit-Baron <1bit.baron@gmail.com>
2022-09-21 20:52:32 +0200

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/elements/Header';
+import DropDown from './components/elements/DropDown';
 
 function App() {
+  const [open, setOpen] = useState();
   return (
     <section className=''>
-      <nav className='px-2 sm:px-4 py-2.5  bg-purple-800'>
+      <nav className='px-2 sm:px-4 py-2.5  mb-5 bg-purple-800'>
         <Header name='DownConvert' />
       </nav>
+      <DropDown />
     </section>
   );
 }
