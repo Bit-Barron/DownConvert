d61df4d4dd4e08a29e24d864e9059147b1c1ca45
Bit-Baron <1bit.baron@gmail.com>
2022-09-21 19:22:49 +0200

    wlke

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import Header from './components/elements/Header';
-import Icon from './images/icon.png';
 
 function App() {
   return (
     <section className=''>
-      <nav className=' border-gray-200 px-2 sm:px-4 py-2.5 rounded bg-purple-800'>
+      <nav className='px-2 sm:px-4 py-2.5  bg-purple-800'>
         <Header name='DownConvert' />
       </nav>
     </section>
