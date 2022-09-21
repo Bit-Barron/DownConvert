cee9ac73b95a84bae4f2660d796b035c889ef05b
Bit-Baron <1bit.baron@gmail.com>
2022-09-21 20:14:31 +0200

    asdasdfafj

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/elements/Header';
 function App() {
   return (
     <section className=''>
-      <nav className='px-2 sm:px-4 py-2.5  bg-purple-800'>
+      <nav className='px-2 sm:px-4 py-2.5 w-1 bg-purple-800'>
         <Header name='DownConvert' />
       </nav>
     </section>
