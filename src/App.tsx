d4cf45e648cbcc002e975af91d086bc20a965fa6
Bit-Baron <1bit.baron@gmail.com>
2022-09-22 19:18:00 +0200

    <cx<ycx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from './components/elements/Header';
 import DropDown from './components/elements/DropDown';
 
 function App() {
-  const [open, setOpen] = useState();
   return (
     <section className=''>
       <nav className='px-2 sm:px-4 py-2.5  mb-5 bg-purple-800'>
