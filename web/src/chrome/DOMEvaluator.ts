431c5cc030c4074152aae78b34ae5e3cc33163c6
Don-Cryptus <don.cryptus@gmail.com>
2022-11-13 15:21:05 +0100

    clean up

--- web/src/chrome/DOMEvaluator.ts
+++
@@ -1,8 +0,0 @@
-export {};
-
-chrome.webRequest.onCompleted.addListener(
-  (details) =>
-    document.dispatchEvent(new CustomEvent('webRequest', { detail: details })),
-  { urls: ['<all_urls>'] },
-  ['responseHeaders', 'extraHeaders']
-);
