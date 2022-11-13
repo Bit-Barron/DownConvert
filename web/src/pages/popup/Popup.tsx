045323ce4507cd62adb378e39e62a7750e455bb0
Don-Cryptus <don.cryptus@gmail.com>
2022-11-13 17:09:36 +0100

    clean up

--- web/src/pages/popup/Popup.tsx
+++ web/src/pages/popup/Popup.tsx
@@ -4,18 +4,18 @@ import React, { useEffect, useState } from "react";
 const Popup: React.FC = () => {
   const [imgs, setImgs] = useState<string[]>([]);
 
-  useEffect(() => {
-    const messageHandler = (request: {
-      msg: string;
-      data?: chrome.webRequest.WebResponseCacheDetails;
-    }) => {
-      const data = request?.data;
-      console.log(request);
-      if (data?.type === "image") {
-        setImgs((imgs) => [...imgs, data.url]);
-      }
-    };
+  const messageHandler = (request: {
+    msg: string;
+    data?: chrome.webRequest.WebResponseCacheDetails;
+  }) => {
+    const data = request?.data;
+    if (data?.type === "image") {
+      setImgs((imgs) => [...imgs, data.url]);
+    }
+  };
 
+
+  useEffect(() => {
     chrome.runtime.onMessage.addListener(messageHandler);
 
     return () => {
@@ -27,7 +27,7 @@ const Popup: React.FC = () => {
 
   return (
     <section className="p-10">
-      <h1 className="font-bold text-lg">Header</h1>
+      <h1 className=" text-lg font-bold">Header</h1>
 
       {imgs?.map((image) => (
         <img key={image} src={image} id={image} alt="images" />
