f54629553b72a18e3fde99d36fe321f4e897b669
Don-Cryptus <don.cryptus@gmail.com>
2022-11-13 16:55:33 +0100

    clean up

---
+++ web/prettier.config.js
@@ -0,0 +1,8 @@
+// prettier.config.js
+module.exports = {
+  plugins: [require("prettier-plugin-tailwindcss")],
+  tailwindConfig: "tailwind.config.js",
+  singleQuote: false,
+  trailingComma: "es5",
+  arrowParens: "always",
+};
