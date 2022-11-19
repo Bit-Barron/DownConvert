0a601b2bfe76b16ae80b45f388e3f8d593277a22
Bit-Barron <1bit.baron@gmail.com>
2022-11-19 02:13:42 +0100

    popup

--- web/src/pages/popup/Popup.tsx
+++ web/src/pages/popup/Popup.tsx
@@ -52,6 +52,11 @@ const Popup: React.FC = () => {
         <div>
           <Tabs />
         </div>
+        <div>
+          <button className="rounded border border-blue-700 bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 mt-5">
+            Select All Images
+          </button>
+        </div>
         {/* <div className="mt-"><Dropdown/></div> */}
         <main className="image-container">
           <div>
@@ -60,12 +65,15 @@ const Popup: React.FC = () => {
               const headers = format?.value.split("/")[1].toUpperCase();
               return (
                 <>
-                  <div className="container mt-6 mb-6 overflow-hidden rounded bg-white shadow-lg " id={image.url}>
+                  <div
+                    className="container mb-6 overflow-hidden rounded bg-white shadow-lg "
+                    id={image.url}
+                  >
                     <img
                       key={url}
                       src={url}
                       alt="images"
-                      className="min-h-full min-w-full cursor-pointer focus:focus-1"
+                      className="focus:border-black min-h-full min-w-full "
                     />
                     <div className="tag">
                       <ul className=" text-black">
