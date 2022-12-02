b741b767000794a7a916a7cf697d9590657edb6a
Bit-Barron <1bit.baron@gmail.com>
2022-12-02 21:17:08 +0100

    app controller

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -16,11 +16,12 @@ export class AppController {
 
   @Post('imgs')
   async getImgUrl(@Body() images: Imgurl[]) {
-    if (images && images.length > 0) {
-      console.log('asasd');
-    } else {
-      for (let image of images) {
-        console.log(image.url);
+    for (let image of Object.keys(images)) {
+      if(images === undefined) {
+        return;
+      } else {
+        const url = images[image].url;
+        console.log(url)
       }
     }
 
@@ -40,8 +41,6 @@ export class AppController {
       response.data.on('error', (err: any) => {
         reject(err);
       });
-
-      console.log(images);
     });
   }
 }
